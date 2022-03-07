61c834c50c69d350d8feb129873aa5bc040798dc
Francoviera-Eldars <franco.viera@eldars.com.ar>
2022-03-07 11:40:00 -0300

    conexion mysql

--- src/main/java/Practica/domain/Persona.java
+++ src/main/java/Practica/domain/Persona.java
@@ -1,15 +1,27 @@
 package Practica.domain;
 
+import java.io.Serializable;
+
+import org.hibernate.annotations.Table;
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
-public class Persona {
+@org.hibernate.annotations.Entity
+public class Persona implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	//@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
 	private String nombre;
 	private String apellido;
 	private String email;
 	private int numero;
 	
+
 	public Persona(String nombre, String apellido, String email, int numero) {
 		super();
 		this.nombre = nombre;
@@ -17,6 +29,23 @@ public class Persona {
 		this.email = email;
 		this.numero = numero;
 	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
 	
 	
 }
